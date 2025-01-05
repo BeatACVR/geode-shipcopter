5bd40146e420258f0030142708adf868e14d7abf
Beat <75857504+BeatACVR@users.noreply.github.com>
2025-01-05 14:50:33 -0600

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(PlayerObject) {
+public:
+    struct Fields {
+        bool m_isSwing;
+        bool m_isUpsideDown;
+    };
+
+    // check if gd is in gameplay, runs when player starts or stops holding
+	bool integrityCheck() {
+        if (!PlayLayer::get()) {
+            if (!LevelEditorLayer::get()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool pushButton(PlayerButton p0) {
+        if (!integrityCheck()) return PlayerObject::pushButton(p0);		
+        if (PlayerObject::pushButton(p0) && m_isSwing) {
+            GJBaseGameLayer::get()->m_player1->flipGravity(m_isUpsideDown, true);
+            GJBaseGameLayer::get()->m_player2->flipGravity(m_isUpsideDown, true); 
+            return true;
+        }
+        return false;
+    }
+
+    bool releaseButton(PlayerButton p0) {
+		if (!integrityCheck()) return PlayerObject::releaseButton(p0);		
+        if (PlayerObject::releaseButton(p0) && m_isSwing) {
+            GJBaseGameLayer::get()->m_player1->flipGravity(!m_isUpsideDown, true);
+            GJBaseGameLayer::get()->m_player2->flipGravity(!m_isUpsideDown, true); 
+            return true;
+        }
+        return false;
+    }
+};
